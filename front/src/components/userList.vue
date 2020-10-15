ec881732cdd404c7a6cffad2344e2fb9712f0219
Giranda22-Git <61464174+Giranda22-Git@users.noreply.github.com>
2020-10-15 21:53:05 +0600

    Update userList.vue

--- front/src/components/userList.vue
+++ front/src/components/userList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
     <ul>
+      <input type="text" class="sort">
       <li
       v-for="(user, index) in users"
       :key="index + Math.random().toString(36).slice(-8)"
@@ -53,6 +54,7 @@ export default {
       width: 100%
       height: 100%
       overflow-y: auto
+      overflow-x: hidden
       display: flex
       flex-direction: column
       justify-content: flex-start
@@ -62,9 +64,37 @@ export default {
         height: 10%
         min-height: 80px
         display: flex
-        justify-content: center
+        justify-content: flex-start
         align-items: center
+        text-align: left !important
+        padding-left: 35%
         cursor: pointer
         &:hover
           background-color: lighten(#000, 20%)
+      .sort
+        width: 80%
+        height: 50%
+        border: none
+        border-bottom: 1px solid
+        background-color: transparent
+        outline: none !important
+        font-size: 2vh
+        font-family: sans-serif
+        transition: color .3s
+        color: aqua
+        padding: 0
+        animation-name: rainbow !important
+        animation-duration: 20s
+        animation-timing-function: linear
+        animation-iteration-count: infinite
+        &:focus
+          color: inherit
+      ::-webkit-input-placeholder
+        color: inherit
+      ::-moz-placeholder
+        color: inherit
+      :-moz-placeholder
+        color: inherit
+      :-ms-input-placeholder
+        color: inherit
 </style>
